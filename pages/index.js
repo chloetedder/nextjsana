6b3fd74988ce982e0b07de8a7b193a0a34a9b0ca
chloetedder <chloe.tedder@vercel.com>
2023-09-13 13:36:05 -0600

    add analytics

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+      <Analytics />
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
